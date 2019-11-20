c09c170d78ead560e536acf1466e3615aff51896
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-20 19:18:37 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,21 +12,25 @@
 class Artist
   attr_accessor :name
   @@all = []
+  
   def initialize(name)
     @name = name
     @@all << self
+    @songlist = []
   end
   def self.all
     @@all
   end
   def add_song(song)
-
+    @songlist << song
+    song.artist = self
   end
   def songs
-
+    @songlist
   end
-  def find_or_create_by_name(name)
-
+  def self.find_or_create_by_name(name)
+    
+  end
   def print_songs
 
  end
