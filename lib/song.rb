0443b82ec5d4924e783ed798a7af38ccbf352f3d
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-20 19:55:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,16 +6,14 @@
 #.new_by_filename associates new song instance with the artist from the filename
 ##artist_name= accepts an artist's name, finds or creates an Artist instance and assigns it to the Song's artist attribut
 class Song
-  attr_accessor :artist, :name, :artist_name
-  @@all = []
+  attr_accessor :name
   def initialize(name)
     @name = name
-    @@all << self
   end
   def self.all
-    @@all
+    
   end
   def artist_name
-    (self.artist.nil?) ? nil : self.artist.name
+    
   end
 end
